5816da4756be4c835029fb550de5b8c25dd869d3
devops_tools <varaprasadkasturi.dba@gmail.com>
2026-08-12 11:57:16 +0530

    Update addons.tf

--- eks-terraform/addons.tf
+++ eks-terraform/addons.tf
@@ -1,1 +1,24 @@
 
+locals {
+  cluster_addons = {
+    coredns = {
+      most_recent = true
+
+    }
+
+    kube-proxy = {
+      most_recent = true
+
+    }
+
+    vpc-cni = {
+      most_recent = true
+
+    }
+
+    aws-ebs-csi-driver = {
+      most_recent              = true
+      service_account_role_arn = aws_iam_role.ebs_csi.arn
+    }
+  }
+}
